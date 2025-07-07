faf3b1b68d096ad6244bca943b356bba0d795c3d
Muhammed Mesut Bahadır <mesutbahadir81@gmail.com>
2025-07-07 15:25:55 +0300

    latest

--- mesutbahadir-com/next.config.ts
+++ mesutbahadir-com/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/mesutbahadir-com',
-  assetPrefix: '/mesutbahadir-com',
+  basePath: '/mesutbahadir1.github.io',
+  assetPrefix: '/mesutbahadir1.github.io',
 };
 
 module.exports = nextConfig;
